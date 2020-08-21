68d52e562a8b1e5372fa93cb90a7ce563443e49d
Ryan De Coster <ryandecoster@gmail.com>
2020-08-20 20:53:34 -0700

    Created add child function and fixed functional shared class

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,11 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  // extended tree method
+  _.extend(newTree, treeMethods);
+  // created an array to hold child treee nodes
+
+  newTree.children = []; // [{},{}]
 
   return newTree;
 };
@@ -11,6 +15,12 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // create new child node using the Tree class
+  var childNode = Tree(value);
+  // passing the value argument to new childnode
+  this.children.push(childNode);
+  // pushing the child node to the children Array
+
 };
 
 treeMethods.contains = function(target) {
